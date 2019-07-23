04922f2224e4df51a5d4a7cf3e21e738e651f2f4
XeoNovaDan <toghill7252010@hotmail.co.uk>
2019-07-23 11:43:18 +0100

    v1.2

--- Source/ProperShotguns/Properties/AssemblyInfo.cs
+++ Source/ProperShotguns/Properties/AssemblyInfo.cs
@@ -4,17 +4,17 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
 [assembly: CompilationRelaxations(8)]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue | DebuggableAttribute.DebuggingModes.DisableOptimizations)]
-[assembly: AssemblyTitle("WeaponTweaks")]
+[assembly: AssemblyTitle("ProperShotguns")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WeaponTweaks")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("ProperShotguns")]
+[assembly: AssemblyCopyright("Copyright © XeoNovaDan 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8f693d16-8eb4-44cd-8d85-ed1ace3c09b5")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
